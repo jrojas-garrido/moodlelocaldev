7fe8c00ba73387d60ee19a14eee0d935d93e2430
unknown <p0081068@CHUM46895.chum.rtss.qc.ca>
2016-04-21 18:13:03 -0400

    Modification de champs de profil que pour les administrateurs editlib.php

--- user/editlib.php
+++ user/editlib.php
@@ -362,9 +362,13 @@ function useredit_shared_definition(&$mform, $editoroptions, $filemanageroptions
         $mform->addElement('select', 'theme', get_string('preferredtheme'), $choices);
     }
 
+    $mform->addElement('header', 'moodle_picture', "Titre d'Emploi et Department");
     $mform->addElement('editor', 'description_editor', get_string('userdescription'), null, $editoroptions);
     $mform->setType('description_editor', PARAM_CLEANHTML);
     $mform->addHelpButton('description_editor', 'userdescription');
+
+    $mform->addElement('text', 'department', get_string('department'), 'maxlength="255" size="25"');
+    $mform->setType('department', PARAM_TEXT);
 
     if (empty($USER->newadminuser)) {
         $mform->addElement('header', 'moodle_picture', get_string('pictureofuser'));
@@ -389,58 +393,63 @@ function useredit_shared_definition(&$mform, $editoroptions, $filemanageroptions
 
     // Display user name fields that are not currenlty enabled here if there are any.
     $disabledusernamefields = useredit_get_disabled_name_fields($enabledusernamefields);
-    if (count($disabledusernamefields) > 0) {
-        $mform->addElement('header', 'moodle_additional_names', get_string('additionalnames'));
-        foreach ($disabledusernamefields as $allname) {
-            $mform->addElement('text', $allname, get_string($allname), 'maxlength="100" size="30"');
-            $mform->setType($allname, PARAM_NOTAGS);
+    /* DEAC_CHUM On enleve les Noms supplémentaires  */
+    if (is_siteadmin()) {
+        if (count($disabledusernamefields) > 0) {
+            $mform->addElement('header', 'moodle_additional_names', get_string('additionalnames'));
+            foreach ($disabledusernamefields as $allname) {
+                $mform->addElement('text', $allname, get_string($allname), 'maxlength="100" size="30"');
+                $mform->setType($allname, PARAM_NOTAGS);
+            }
+        }
+
+        if (!empty($CFG->usetags) and empty($USER->newadminuser)) {
+            $mform->addElement('header', 'moodle_interests', get_string('interests'));
+            $mform->addElement('tags', 'interests', get_string('interestslist'), array('display' => 'noofficial'));
+            $mform->addHelpButton('interests', 'interestslist');
         }
     }
 
-    if (!empty($CFG->usetags) and empty($USER->newadminuser)) {
-        $mform->addElement('header', 'moodle_interests', get_string('interests'));
-        $mform->addElement('tags', 'interests', get_string('interestslist'), array('display' => 'noofficial'));
-        $mform->addHelpButton('interests', 'interestslist');
-    }
+    /* DEAC_CHUM On enleve les elements optionel sauf le departement  */
+        // Moodle optional fields.
+        $mform->addElement('header', 'moodle_optional', get_string('optional', 'form'));       
 
-    // Moodle optional fields.
-    $mform->addElement('header', 'moodle_optional', get_string('optional', 'form'));
+        if (is_siteadmin()) {
 
-    $mform->addElement('text', 'url', get_string('webpage'), 'maxlength="255" size="50"');
-    $mform->setType('url', PARAM_URL);
+            $mform->addElement('text', 'url', get_string('webpage'), 'maxlength="255" size="50"');
+            $mform->setType('url', PARAM_URL);
 
-    $mform->addElement('text', 'icq', get_string('icqnumber'), 'maxlength="15" size="25"');
-    $mform->setType('icq', PARAM_NOTAGS);
+            $mform->addElement('text', 'icq', get_string('icqnumber'), 'maxlength="15" size="25"');
+            $mform->setType('icq', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'skype', get_string('skypeid'), 'maxlength="50" size="25"');
-    $mform->setType('skype', PARAM_NOTAGS);
+            $mform->addElement('text', 'skype', get_string('skypeid'), 'maxlength="50" size="25"');
+            $mform->setType('skype', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'aim', get_string('aimid'), 'maxlength="50" size="25"');
-    $mform->setType('aim', PARAM_NOTAGS);
+            $mform->addElement('text', 'aim', get_string('aimid'), 'maxlength="50" size="25"');
+            $mform->setType('aim', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'yahoo', get_string('yahooid'), 'maxlength="50" size="25"');
-    $mform->setType('yahoo', PARAM_NOTAGS);
+            $mform->addElement('text', 'yahoo', get_string('yahooid'), 'maxlength="50" size="25"');
+            $mform->setType('yahoo', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'msn', get_string('msnid'), 'maxlength="50" size="25"');
-    $mform->setType('msn', PARAM_NOTAGS);
+            $mform->addElement('text', 'msn', get_string('msnid'), 'maxlength="50" size="25"');
+            $mform->setType('msn', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'idnumber', get_string('idnumber'), 'maxlength="255" size="25"');
-    $mform->setType('idnumber', PARAM_NOTAGS);
+            $mform->addElement('text', 'idnumber', get_string('idnumber'), 'maxlength="255" size="25"');
+            $mform->setType('idnumber', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'institution', get_string('institution'), 'maxlength="255" size="25"');
-    $mform->setType('institution', PARAM_TEXT);
+            $mform->addElement('text', 'institution', get_string('institution'), 'maxlength="255" size="25"');
+            $mform->setType('institution', PARAM_TEXT);
 
-    $mform->addElement('text', 'department', get_string('department'), 'maxlength="255" size="25"');
-    $mform->setType('department', PARAM_TEXT);
+            $mform->addElement('text', 'phone1', get_string('phone1'), 'maxlength="20" size="25"');
+            $mform->setType('phone1', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'phone1', get_string('phone1'), 'maxlength="20" size="25"');
-    $mform->setType('phone1', PARAM_NOTAGS);
+            $mform->addElement('text', 'phone2', get_string('phone2'), 'maxlength="20" size="25"');
+            $mform->setType('phone2', PARAM_NOTAGS);
 
-    $mform->addElement('text', 'phone2', get_string('phone2'), 'maxlength="20" size="25"');
-    $mform->setType('phone2', PARAM_NOTAGS);
+            $mform->addElement('text', 'address', get_string('address'), 'maxlength="255" size="25"');
+            $mform->setType('address', PARAM_TEXT);
+        }
 
-    $mform->addElement('text', 'address', get_string('address'), 'maxlength="255" size="25"');
-    $mform->setType('address', PARAM_TEXT);
 }
 
 /**
